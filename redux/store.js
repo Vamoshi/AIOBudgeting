264501edd94362ffaa5be5c235bdc1b083ffa98e
Vamoshi <64162877+Vamoshi@users.noreply.github.com>
2023-12-13 14:26:33 -0800

    Revert "Add Redux"
    This reverts commit bee006a926b3ee95563af6c27e99a4d739df02a9.

--- redux/store.js
+++
@@ -1,7 +0,0 @@
-import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux"
-import thunk from "redux-thunk"
-import searchReducer from "./reducers"
-
-const rootReducer = combineReducers({ searchReducer })
-
-export const Store = createStore(rootReducer, applyMiddleware(thunk))
